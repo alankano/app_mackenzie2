3218404d00f9b5c0390bf63dfadcd85f28bba44d
Alan Kano <38501151+alankano@users.noreply.github.com>
2020-11-23 18:55:51 -0300

    Add files via upload

---
+++ banco_de_exercicio.py
@@ -0,0 +1,213 @@
+dict_exercicios ={
+    0 : {
+        'enunciado' : 'Determine o valor de m na equação x² – (m + 5)x + 36 = 0, de modo que as raízes sejam reais e diferentes.',
+        'alternativas' :{'a' : '7 ou -17',
+                         'b' : '8 ou 18',
+                         'c' : '7 ou 18 ',
+                         'd' : '7 ou -18',
+                         'e' : '8 ou - 17'
+                         },
+        'resposta_correta' : 'a'
+    },
+    1 : {
+        'enunciado' : 'Quais são as raízes da equação x²-x-20=0?',
+        'alternativas' : {'a' :'S={-4 e 5}',
+                          'b' :'S={7 e 9}',
+                          'c' :'S={-25 e 75)',
+                          'd' :'S={-7 e 4}',
+                          'e' :'S={6 e 8}'
+        },  
+        'resposta_correta' : 'a'
+        },
+    2: {
+        'enunciado' :'Quais são as raízes da equação x²-3x-4=0?',
+        'alternativas' : { 'a' :'S={1 e 7}',
+                           'b' :'S={9 e 14}',
+                           'c' :'S={2 e 8}',
+                           'd' :'S={-1 e 4}',
+                           'e' :'S={3 e 4}' 
+        },
+        'resposta_correta' : 'd'
+
+    },
+    3 : {
+        'enunciado' : 'Quais são as raízes da equação x²-14x+48=0?',
+        'alternativas' :{'a' : 'S={9 e 10}',
+                         'b' : 'S={4 e 8}',
+                         'c' : 'S={6 e 8}',
+                         'd' : 'S={1 e 5}',
+                         'e' : 'S={-6 e -4}'
+                         },
+        'resposta_correta' : 'c'
+    },
+    4 : {
+        'enunciado' : 'Escolha qual das alternativas correspondem as letras a, b e c, da equação x²+9x+8=0?',
+        'alternativas' :{'a' : 'a=1, b=2 e c=7',
+                         'b' : 'a=1, b=5 e c=-8',
+                         'c' : 'a=-1, b=-9 e c=4',
+                         'd' : 'a=2, b=18 e c=16',
+                         'e' : 'a=1, b=9 e c=8'
+                         },
+        'resposta_correta' : 'e'
+    },
+    5 : {
+        'enunciado' : 'Escolha qual das alternativas correspondem as letras a, b e c, da equação x²-7x+5=0?',
+        'alternativas' :{'a' : 'a=-1, b=7 e c=-8',
+                         'b' : 'a=1, b=-7 e c=5',
+                         'c' : 'a=1, b=-2 e c=12',
+                         'd' : 'a=1, b=-3 e c=1',
+                         'e' : 'a=4, b=-8 e c=9'
+                         },
+        'resposta_correta' : 'b'
+    },
+    6 : {
+        'enunciado' : 'Quais são as raízes da equação x²-5x+6=0?',
+        'alternativas' :{'a' : 'S={-2 e 4}',
+                         'b' : 'S={1 e 9}',
+                         'c' : 'S={2 e 3}',
+                         'd' : 'S={8 e 13}',
+                         'e' : 'S={-2 e 4}'
+                         },
+        'resposta_correta' : 'c'
+    },
+    7 : {
+        'enunciado' : 'Quais são as raízes da equação x²+2x-8=0?',
+        'alternativas' :{'a' : 'S={1 e -5}',
+                         'b' : 'S={7 e -1}',
+                         'c' : 'S={1 e -1}',
+                         'd' : 'S={2 e -4}',
+                         'e' : 'S={-2 e -7}'
+                         },
+        'resposta_correta' : 'd'
+    },
+    8 : {
+        'enunciado' : 'Quais são as raízes da equação x²+3x-28=0?',
+        'alternativas' :{'a' : 'S={-7 e 4}',
+                         'b' : 'S={-1 e 4}',
+                         'c' : 'S={8 e -7}',
+                         'd' : 'S={-7 e 7}',
+                         'e' : 'S={5 e -4}'
+                         },
+        'resposta_correta' : 'a'
+    },
+    9 : {
+        'enunciado' : 'Escolha qual das alternativas correspondem as letras a, b e c, da equação x²+5x+6=0?',
+        'alternativas' :{'a' : 'a=2, b=-5 e c=7',
+                         'b' : 'a=-1, b=-5 e c=-6',
+                         'c' : 'a=1, b=5 e c=6',
+                         'd' : 'a=9, b=3 e c=7',
+                         'e' : 'a=1, b=3 e c=5'
+                         },
+        'resposta_correta' : 'c'
+    },
+    10 : {
+        'enunciado' : 'Resolva a equação do 2° grau 2x²+x–3 = 0.',
+        'alternativas' :{'a' : 'S={0 e 2}',
+                         'b' : 'S={3 e 4}',
+                         'c' : 'S={1 e 1,5}',
+                         'd' : 'S={-1 e 1}',
+                         'e' : 'S={1 e 5}'
+                         },
+        'resposta_correta' : 'c'
+    },
+    11 : {
+        'enunciado' : 'As duas soluções de uma equação do 2° grau são – 1 e 1/3. Então a equação é:',
+        'alternativas' :{'a' : '3x² – x – 1 = 0',
+                         'b' : '3x² + x – 1 = 0',
+                         'c' : '3x² + 2x – 1 = 0',
+                         'd' : '3x² – 2x – 2 = 0',
+                         'e' : '3x² – x + 1 = 0'
+                         },
+        'resposta_correta' : 'c'
+    },
+    12 : {
+        'enunciado' : 'A maior raiz da equação – 2x² + 3x + 5 = 0 vale:',
+        'alternativas' :{'a' : '– 1',
+                         'b' : '1',
+                         'c' : '2',
+                         'd' : '2,5',
+                         'e' : '(3 + √19)/4'
+                         },
+        'resposta_correta' : 'd'
+    },
+    12 : {
+        'enunciado' : 'A maior raiz da equação – 2x² + 3x + 5 = 0 vale:',
+        'alternativas' :{'a' : '– 1',
+                         'b' : '1',
+                         'c' : '2',
+                         'd' : '2,5',
+                         'e' : '(3 + √19)/4'
+                         },
+        'resposta_correta' : 'd'
+    },
+    13 : {
+        'enunciado' : 'Se v e w são as raízes da equação x² + ax + b = 0, em que a e b são coeficientes reais, então v² + w² é igual a:',
+        'alternativas' :{'a' : 'a² - 2b',
+                         'b' : 'a² + 2b',
+                         'c' : 'a² – 2b²',
+                         'd' : 'a² + 2b²',
+                         'e' : 'a² – b²'
+                         },
+        'resposta_correta' : 'a'
+    },
+    14 : {
+        'enunciado' : 'Laura tem de resolver uma equação do 2º grau no “para casa”, mas percebe que, ao copiar do quadro para o caderno, esqueceu-se de copiar o coeficiente de x. Para resolver a equação, registrou-a da seguinte maneira: 4x² + ax + 9 = 0. Como ela sabia que a equação tinha uma única solução, e esta era positiva, conseguiu determinar o valor de a, que é',
+        'alternativas' :{'a' : '– 13',
+                         'b' : '– 12',
+                         'c' : '12',
+                         'd' : '13',
+                         'e' : '11'
+                         },
+        'resposta_correta' : 'c'
+    },
+    15 : {
+        'enunciado' : 'Para qual valor de “a” a equação (x – 2) * (2ax – 3) + (x – 2) * (- ax + 1) = 0 tem duas raízes e iguais?',
+        'alternativas' :{'a' : '-1',
+                         'b' : '0',
+                         'c' : '1',
+                         'd' : '2',
+                         'e' : '-2'
+                         },
+        'resposta_correta' : 'c'
+    },
+    16 : {
+        'enunciado' : 'Para qual valor de “a” a equação (x – 2) * (2ax – 3) + (x – 2) * (- ax + 1) = 0 tem duas raízes e iguais?',
+        'alternativas' :{'a' : 'um número par.',
+                         'b' : 'um número ímpar.',
+                         'c' : 'um número primo.',
+                         'd' : 'um número divisível por 3.',
+                         'e' : 'um quadrado perfeito.'
+                         },
+        'resposta_correta' : 'e'
+    },
+    17 : {
+        'enunciado' : 'Dada a equação x4 + 4x2 - 45=0, podemos afirmar que:',
+        'alternativas' :{'a' : 'tal equação possui 4 raízes reais.',
+                         'b' : 'duas de suas raízes são números racionais.',
+                         'c' : 'a soma das suas raízes reais é igual a −4.',
+                         'd' : 'o produto das suas raízes reais é igual a −5.',
+                         'e' : 'o produto das suas raízes reais é igual a −45.'
+                         },
+        'resposta_correta' : 'd'
+    },
+    18 : {
+        'enunciado' : 'Se x é um número real positivo tal que ao adicionarmos 1 ao seu inverso obtemos como resultado o número x, qual é o valor de x?',
+        'alternativas' :{'a' : '(1 – √5)/2',
+                         'b' : '(1 + √5)/2',
+                         'c' : '1',
+                         'd' : '(1 + √3)2',
+                         'e' : '(1 – √5)/2 '
+                         },
+        'resposta_correta' : 'b'
+    },
+    19 : {
+        'enunciado' : 'Se uma das raízes da equação 2x² – 3px + 40 = 0 é 8, determine o valor de p.',
+        'alternativas' :{'a' : '7',
+                         'b' : '8',
+                         'c' : '9',
+                         'd' : '10',
+                         'e' : '6'
+                         },
+        'resposta_correta' : 'a'
+    }
+}
---
+++ minha_materia.py
@@ -0,0 +1,67 @@
+primeira_string = '''
+A equação do segundo grau, também chamada de equação quadrática, é expressada por:
+
+ax²+bx+c=0
+Observe que o maior grau da equação é dado pelo expoente do termo ax², por esse motivo a equação é do segundo grau. Chamamos de incógnita os valores desconhecidos como x, y e z. Já a, b e c são números reais chamados de coeficientes.
+
+As equações do segundo grau podem ser do tipo completa ou incompleta. O que determina isso são os coeficientes b e c. Lembre-se sempre que o coeficiente a deve ser diferente de zero, caso não seja a equação não será do segundo grau. Veja como podemos representar a equação completa e incompleta.
+
+Equação do segundo grau completa
+Para uma equação ser considerada completa, os coeficientes a, b e c, devem ser diferentes de zero, ou seja: a ≠ 0, b ≠ 0 e c ≠ 0. Com isso a equação do segundo grau possui a seguinte estrutura: ax²+bx+c=0. Acompanhe os exemplos a seguir:
+
+2x²+4x+1=0
+a = 2, b = 4, c = 1
+
+y²+3y+6=0
+a = 1, b = 3, c = 6
+'''
+segunda_string = '''
+Equação do segundo grau incompleta
+Quando a equação é incompleta os coeficientes b e c podem ser iguais a zero, ou seja, b = 0 e c = 0. Veja os exemplos:
+
+x²−4=0
+a = 1, b = 0, c = - 4
+
+3y²+6x=0
+a = 3, b = 6, c = 0
+
+5z²=0
+a = 5, b = 0, c = 0
+'''
+terceira_string = '''
+Resolvendo a equação do segundo grau
+O objetivo de se resolver uma equação do segundo grau é encontrar os valores reais que a incógnita assume. Esses valores são chamados de raízes da equação, como ela é do segundo grau deve possuir duas raízes reais diferentes ou idênticas.
+
+Fórmula de Bhaskara
+Para solucionarmos a equações do segundo grau seja ela completa ou incompleta, podemos utilizar a seguinte fórmula:
+
+x=−b± √(b²−4ac)/2a
+Fórmula de Bhaskara completa
+
+A fórmula de Bhaskara pode ser escrita de forma resumida, explicitando o discriminante, ou seja, delta (Δ).
+
+Δ=b²−4ac
+Fórmula do discriminante (Δ)
+'''
+
+quarta_string = '''
+x=−b±√(Δ)/2a
+Fórmula de Bhaskara Resumida
+
+Aplicando a fórmula de Bhaskara
+Exemplo: Resolva a equação do segundo grau: 4x² + 4x + 1 = 0, utilizando a formula de Bhaskara resumida.
+
+4x² + 4x + 1 = 0
+a = 4, b = 4, c = 1
+
+Δ=b²−4ac
+Δ=4²−4*4*1
+Δ=16−16=0
+x=−b±√(Δ)/2a
+x=−4±√(0)/2*4
+x=−4+(0)/8  e x = −4-(0)/8
+As raízes dessa equação do segundo grau são idênticas, sendo assim:
+
+x′=−12
+x′′=−12
+'''
